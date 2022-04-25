9297ff2ba76c03529e8cb3160fbfa5504b2a1400
Aaron Garay <aaronroman93@hotmail.com>
2022-04-25 19:11:35 -0300

    Modifico el atributo alias por nombreUsuario en la entidad y agrego nuevos metodos en el usuarioServicio

--- src/main/java/com/egg/proyecto/repositorios/UsuarioRepositorio.java
+++ src/main/java/com/egg/proyecto/repositorios/UsuarioRepositorio.java
@@ -2,9 +2,16 @@ package com.egg.proyecto.repositorios;
 
 import com.egg.proyecto.entidades.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsuarioRepositorio extends JpaRepository<Usuario, String>{
     
+    @Query("SELECT u FROM Usuario u WHERE u.email = :email")
+    public Usuario buscarUsuarioPorEmail(@Param("email") String email);
+    
+    @Query("SELECT u FROM Usuario u WHERE u.nombreUsuari = :nombreUsuario")
+    public Usuario buscarUsuarioPorNombre(@Param("nombreUsuario") String nombreUsuario);
 }
