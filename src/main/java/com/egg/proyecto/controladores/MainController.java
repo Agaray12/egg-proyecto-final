022adab314a3f0910c93590c672f1f8ed69477c2
Gaston Contreras <gastongastongg@gmail.com>
2022-05-02 17:57:21 -0300

    controlador del poema

--- src/main/java/com/egg/proyecto/controladores/MainController.java
+++ src/main/java/com/egg/proyecto/controladores/MainController.java
@@ -1,31 +1,21 @@
 package com.egg.proyecto.controladores;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
-@RequestMapping("/")
 public class MainController {
-    
     @GetMapping("/")
-    public String index(@RequestParam(required = false) String login, ModelMap model) {
-        if(login != null){
-            model.put("exito", "Logueado con exito");
-        }
+    public String index() {
         return "index";
     }
     @GetMapping("/login")
-    public String login(@RequestParam(required = false) String error, @RequestParam(required = false) String logout, ModelMap model){
-        if(error != null){
-            model.put("error", "Usuario o contraseña incorrectos");
-        }
-        if(logout != null){
-            model.put("logout", "Desconectado correctamente");
-        }
-        return "login";
+    public String login(){
+       return "login";
+    }
+     @GetMapping("/profile")
+    public String profile(){
+       return "profile";
     }
 }
