7352043b85ada9f9afe21183a6dededca82d3333
Gaston Contreras <gastongastongg@gmail.com>
2022-05-12 22:07:48 -0300

    registro

--- src/main/java/com/egg/proyecto/controladores/MainController.java
+++ src/main/java/com/egg/proyecto/controladores/MainController.java
@@ -18,6 +18,10 @@ public class MainController {
     public String login(){
        return "login";
     }
+       @GetMapping("/registro")
+    public String registro(){
+       return "registro";
+    }
     
     @GetMapping("/profile")
     public String perfil(Model model, HttpSession session){
